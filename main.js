a6c7f2b746e9117c4735716d953290e1569378b1
PJOSSE <93270475+PJOSSE@users.noreply.github.com>
2021-11-16 15:17:41 +0100

    essai

--- main.js
+++ main.js
@@ -1,106 +1,58 @@
-/* 
-// Étape 1
-class Clock extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Bonjour, monde !</h1>
-          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
+// Correction exo6
+
+function Clock() {
+  React.useEffect(() => {
+    tick();
+  }, []);
+
+const [date, setDate] = React.useState(new Date());
+const [color, setColor] = React.useState('black');
+const intervalRef = React.useRef();
+
+// React.useEffect(() => {
+//   tick();
+//   // do stuff
+// }, []); // Déclenché a CHAQUE rendu => componentWillMount
+
+// const [date, setDate] = React.useDate(new Date());
+// const [color, setColor] = React.useState('black')
+
+const tick = () => {
+  setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+};
+
+const changeColor = () => {
+  let number =(1<<24) - 1;
+
+  number = Math.floor(Math.random() * number).toString(16);
+    setColor(`#${number}`);
+}
+
+const resetColor = () => {
+setColor ('#000000');
+}
+
+const stopClock = () => {
+  clearInterval(intervalRef.current);
   }
 
-ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
+// const bidule = (e) =>{
+//   e.target.style.color = 'red'
+// }
 
-*/
-
-
-/* 
-
-// Étape 2
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-  render() {
-    return (
-      <div>
-        <h1>Bonjour, monde !</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+return (
+  <div>
+    <h1>Hello world </h1>
+    <h2 style={{color}}>Il est {date.toLocaleTimeString()}.</h2>
+    <button onClick={changeColor}>Random color</button>
+    <button onClick={resetColor}>Reset color</button>
+    <br/>
+    <button onClick={stopClock}>Stop clock</button>
+    <button onClick={tick}>Restart clock</button>
+  </div>
+);
 }
 
-ReactDOM.render(<Clock />, document.querySelector('#app')); 
-
-
- */
-
-
-
-// Étape 3
-
-/* class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render() {
-        return (
-        <div>
-            <h1>Bonjour, monde !</h1>
-            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-        );
-    }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));  */
-
-
-function Clock(props) {
-    React.useEffect(() => {
-        tick();        
-    }, []);
-    
-    const [date, setDate] = React.useState(new Date());
-
-    const tick = () => {
-        setInterval(() => {
-            setDate(new Date())
-        }, 1000)
-    }
-    
-
-    return (
-        <div>
-            <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
-        </div>
-        );
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));
-
-
-
-
-
+ReactDOM.render(<Clock />, document.getElementById("app"));
