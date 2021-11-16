1c491a837e6ae8e91f017a76e4da0e214b0b1704
PJOSSE <93270475+PJOSSE@users.noreply.github.com>
2021-11-16 11:22:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// class Car extends React.Component {
+// Class component
+//class Car extends React.Component {
 //   render() {
 //     return <h2>Hi, I am a Car!</h2>;
 //   }
@@ -8,8 +9,19 @@ const firstName = 'toto';
 const lastName = 'glandu';
 
 class FirstName extends React.Component {
-render() {
-    /*
+
+//     ......bon si je comprend bien, il y a useState et  useEffect. à utiliser. 
+//    l'un useState sert à renvoyer une valeur d’état local et une fonction pour la mettre à jour.
+// const [state, setState] = useState(initialState);
+// Renvoie une valeur d’état local et une fonction pour la mettre à jour.
+
+// Pendant le rendu initial, l’état local (state) a la même valeur que celle passée en premier argument (initialState).
+
+// La fonction setState permet de mettre à jour l’état local. Elle accepte une nouvelle valeur d’état local et planifie un nouveau rendu du composant.
+// Au cours des rendus suivants, la première valeur renvoyée par useState sera toujours celle de l’état local le plus récent, une fois les mises à jour effectuées.
+   
+    
+        /*
     // Solution avec bonus
     const formatFirstName = (firstName) => {
         return firstName[0].toUpperCase() + firstName.substr(1);
@@ -17,12 +29,22 @@ render() {
 
     return <span>{formatFirstName(props.text)}</span>
     */
-
     // Solution sans bonus
+    
+    const [state, setState] = useState(initialState);
+    useEffect(() => {
+    alert(`Bonjour {Firstname}`)
+});
+    render() {
     return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
 }
-}
+} setState(newState);
+
 class LastName extends React.Component {
+     const [state, setState] = useState(initialState);
+    useEffect(() => {
+    alert(`Bonjour {Firstname}`)
+});
 render() {
     /*
     // Solution avec bonus
